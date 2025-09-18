06d817318e2be1557649e0fe6ce1b5e35eec9017
master312 <sythinian@gmail.com>
2025-09-18 18:42:41 +0200

    Added 'play stream' demo page.

---
+++ src/main/webapp/demos/play-demo-config.js
@@ -0,0 +1,15 @@
+
+export const playDemoBackendUrl = "wss://test.antmedia.io/24x7test/websocket";
+
+export const PlayDemoConfig = [
+    {
+        title: 'Demo stream 1',
+        streamId: '24x7-2test',
+        logo: "../img/samples/play_webrtc.png"
+    },
+    {
+        title: 'Demo stream 2',
+        streamId: '24x7test',
+        logo: "../img/samples/play_webrtc.png"
+    },
+]; 
